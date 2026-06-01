631afc800ece0b934809a8fb91c55814896ecde5
Matheus <matheus2003pl@gmail.com>
2026-06-01 20:10:07 -0300

    feat: adicao de feature de excluir descricao

--- projeto_a3/apps/description/urls.py
+++ projeto_a3/apps/description/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('adicionar/', views.add_description, name='add_description'),
     path('listar/', views.list_descriptions, name='list_descriptions'),
     path('editar/<int:id_description>/', views.edit_description, name='edit_description'),
+    path('excluir/<int:id_description>/', views.delete_description, name='delete_description'),
 ]
